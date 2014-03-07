ed70374013b12239c9ba934d3587b4d442c9320a
mshahriarinia <info@mshahriarinia.com>
2014-03-07 08:33:29 -0800

    Walk through of rotten potatoe sections 4.1-4.3  https://www.youtube.com/watch?v=5ar8SWO1lGs&list=PLjbL0BCR04Q13ph3TLp3dPsYQi7ujH6R4&index=8

---
+++ 1x/hw2/myrottenpotatoes/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpotatoes::Application.config.secret_token = 'c3e8f4639efbad6de25f7b2d9649ca4c60bed534aa976a4d121ae5ab7418f70719c21e57eefd23af2ab24e3329bf028ece1c65051be19dcf1d9ca9f5ac8a2d52'
