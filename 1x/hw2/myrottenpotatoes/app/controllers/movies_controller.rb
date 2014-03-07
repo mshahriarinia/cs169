d2f942ff1569f4782d05848bdb197c8fcfb72da5
mshahriarinia <info@mshahriarinia.com>
2014-03-07 08:51:19 -0800

    created show action. Converted application.html.erb to application.html.haml

--- 1x/hw2/myrottenpotatoes/app/controllers/movies_controller.rb
+++ 1x/hw2/myrottenpotatoes/app/controllers/movies_controller.rb
@@ -2,4 +2,9 @@ class MoviesController < ApplicationController
   def index
     @movies =  Movie.all
   end
+
+  def show
+    id = params[:id]    # e.g. 1 is the id of the movie will be here  movies/1
+    @movie = Movie.find(id)
+  end
 end
