592e728a5db80c631f8a4968d4bbcfed08215ae7
mshahriarinia <info@mshahriarinia.com>
2014-03-07 09:55:42 -0800

    Show details of model (movie)

--- 1x/hw2/myrottenpotatoes/app/controllers/movies_controller.rb
+++ 1x/hw2/myrottenpotatoes/app/controllers/movies_controller.rb
@@ -6,5 +6,8 @@ class MoviesController < ApplicationController
   def show
     id = params[:id]    # e.g. 1 is the id of the movie will be here  movies/1
     @movie = Movie.find(id)
+    #@movie = Movie.find_by_title(id)    
+    # you can go to /movies/Star Wars Morty, btu also you need to fix the index template to generate url of 
+    # model by title -that piece not done in this iteration
   end
 end
